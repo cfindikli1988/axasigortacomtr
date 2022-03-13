266855254b6387bf3ce6510e766dd96f61b0d9a0
Caglar FINDIKLI <cfindikli@outlook.com>
2022-03-13 20:11:38 +0300

    Update test_axasigortacomtr.py

--- test_axasigortacomtr.py
+++ test_axasigortacomtr.py
@@ -1910,7 +1910,7 @@ def test_checkpoint317():
 def test_checkpoint318():
     url = 'https://www.axasigorta.com.tr/kampanyalar/cep-telefonu-sigortasinda-yuzde-5-paracik'
     response = requests.get(url, verify=False)
-    assert response.status_code == 404
+    assert response.status_code == 200
 
 
 def test_checkpoint319():
@@ -1934,7 +1934,7 @@ def test_checkpoint321():
 def test_checkpoint322():
     url = 'https://www.axasigorta.com.tr/kaza-aninda'
     response = requests.get(url, verify=False)
-    assert response.status_code == 404
+    assert response.status_code == 200
 
 
 def test_checkpoint323():
